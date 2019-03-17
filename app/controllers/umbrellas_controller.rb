ac070885d6ef6782c32fafe1ff414071b184f91d
jkojro <janusz.kojro@sparksoluitons.co>
2019-03-17 15:14:54 +0100

    Add Umbrella message form

--- app/controllers/umbrellas_controller.rb
+++ app/controllers/umbrellas_controller.rb
@@ -1,13 +1,21 @@
 class UmbrellasController < ApplicationController
+  MESSAGE = 'To jest defaultowy message, który będzie wyświetlany w /new, ale może być zmieniony przez usera'.freeze
   def new
     @umbrella = Umbrella.new
+    @umbrella.message = MESSAGE
   end
 
   def create
-    Umbrella.create(params[:id])
+    Umbrella.create(umbrella_params)
   end
 
   def show
     @umbrella = Umbrella.find(params[:id])
+  end
+
+  private
+
+  def umbrella_params
+    params.require(:umbrella).permit(:message)
   end
 end
