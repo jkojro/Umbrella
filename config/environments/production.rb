4cbdb715ae4159e02e770d8b381813131b3b7cd1
jkojro <janusz.kojro@sparksoluitons.co>
2019-03-25 19:45:18 +0100

    Change production host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #Needed for devise gem
-  config.action_mailer.default_url_options = { host: 'https://uhmbrella.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://pink-umbrella.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
