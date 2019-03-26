0597691082530ac0a10358c1900e8051b36b2a1d
jkojro <janusz.kojro@sparksoluitons.co>
2019-03-26 20:36:00 +0100

    Add SendResultsService spec

--- app/services/send_results_service.rb
+++ app/services/send_results_service.rb
@@ -7,8 +7,9 @@ class SendResultsService
   def call
     umbrella = Umbrella.find(@umbrella_id)
 
-    # Return because result already sent when all frineds answeard
-    return if umbrella.friends.all? { |f| f.answear.present? } && umbrella.days_to_end.zero?
+    # If both true => result was already sent
+    # If both false => it's not time yet for sending results
+    return if umbrella.friends.all? { |f| f.answear.present? } == umbrella.days_to_end.zero?
     UmbrellaMailer.send_results(umbrella).deliver_now
   end
 end
---
+++ spec/services/send_results_service_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+describe SendResultsService, type: :service do
+  describe '#call' do
+    subject { described_class.new(umbrella.id).call }
+
+    context 'friends didnt answear and time is end' do
+      let(:umbrella) { create(:umbrella, created_at: 7.days.ago) }
+      let!(:friend) { create(:friend, umbrella_id: umbrella.id, answear: nil) }
+
+      it 'should should call UmbrellaMailer' do
+        expect { subject }.to change { ActionMailer::Base.deliveries.size }.by(1)
+      end
+    end
+
+    context 'friends answeard before and now time is end' do
+      let(:umbrella) { create(:umbrella, created_at: 7.days.ago) }
+      let!(:friend) { create(:friend, umbrella_id: umbrella.id) }
+
+      it 'should not call UmbrellaMailer' do
+        expect { subject }.to change { ActionMailer::Base.deliveries.size }.by(0)
+      end
+    end
+
+    context 'friends answeard and time is not end yet' do
+      let(:umbrella) { create(:umbrella) }
+      let!(:friend) { create(:friend, umbrella_id: umbrella.id) }
+      it 'should call UmbrellaMailer' do
+        expect { subject }.to change { ActionMailer::Base.deliveries.size }.by(1)
+      end
+    end
+
+    context 'friends didnt answear and time is not end yet' do
+      let(:umbrella) { create(:umbrella) }
+      let!(:friend) { create(:friend, umbrella_id: umbrella.id, answear: nil) }
+
+      it 'should not call UmbrellaMailer' do
+        expect { subject }.to change { ActionMailer::Base.deliveries.size }.by(0)
+      end
+    end
+  end
+end
