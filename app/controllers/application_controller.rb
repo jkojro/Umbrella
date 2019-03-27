b26f35b4fb07b12b32d40e11d66717bb1688e6e4
jkojro <janusz.kojro@sparksoluitons.co>
2019-03-27 11:05:45 +0100

    Add 404 NotFound exception handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  rescue_from StandardError, with: -> { render_error 500 }
+
+  rescue_from ActiveRecord::RecordNotFound, with: -> { render_error 404 }
+
+  private
+
+  def render_error(status_number)
+    render file: "#{Rails.root}/public/#{status_number}", layout: 'layouts/application', status: status_number
+  end
 end
